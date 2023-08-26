a7dde0ac771cc0734187a223cd38fc633cfc33be
Joenell Canlas Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-08-26 09:07:17 +0800

    Styled home section

--- assets/main.js
+++ assets/main.js
@@ -8,3 +8,11 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times')
     navbar.classList.toggle('active')
 }
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('active')
+}
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active')
+}
